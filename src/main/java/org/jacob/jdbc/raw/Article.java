da41805164b8608a30371b421adb5db3c0aebc50
w0nse0k <w0nse0k@naver.com>
2019-03-09 22:06:52 +0900

    toString 수정

--- src/main/java/org/jacob/jdbc/raw/Article.java
+++ src/main/java/org/jacob/jdbc/raw/Article.java
@@ -1,70 +1,64 @@
 package org.jacob.jdbc.raw;
 
 public class Article {
-    String articleId;
-    String title;
-    String content;
-    String userId;
-    String name;
-    String cdate;
+	String articleId;
+	String title;
+	String content;
+	String userId;
+	String name;
+	String cdate;
 
-    public String getArticleId() {
-        return articleId;
-    }
+	public String getArticleId() {
+		return articleId;
+	}
 
-    public void setArticleId(String articleId) {
-        this.articleId = articleId;
-    }
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public String getUserId() {
-        return userId;
-    }
+	public String getUserId() {
+		return userId;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getCdate() {
-        return cdate;
-    }
+	public String getCdate() {
+		return cdate;
+	}
 
-    public void setCdate(String cdate) {
-        this.cdate = cdate;
-    }
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "articleId=" + articleId +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", userId='" + userId + '\'' +
-                ", name='" + name + '\'' +
-                ", cdate='" + cdate + '\'' +
-                '}' + '\n';
-    }
+	@Override
+	public String toString() {
+		return "Article [articleId=" + articleId + ", title=" + title + ", content=" + content + ", userId="
+				+ userId + ", name=" + name + ", cdate=" + cdate + "]";
+	}
 }
